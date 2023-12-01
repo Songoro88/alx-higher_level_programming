782b34ed38088b5d925bfb68e3b8bec2ec319837
Songoro88 <songoro88@gmail.com>
2023-12-01 14:23:01 +0300

    10-add.py

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+
+def add(c, d):
+    return (c + d)
---
+++ 0x01-python-if_else_loops_functions/10-main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+Add = __import__('10-add').Add
+
+print(Add(1, 2))
+print(Add(98, 0))
+print(Add(100, -2))
