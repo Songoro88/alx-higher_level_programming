e5e9554b9192bed037d9904f16c80192e8025edb
Songoro88 <songoro88@gmail.com>
2023-12-05 13:45:06 +0300

    2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-def replace_in_list(My_list, idx, ELEMENT):
-    if 0 <= idx < len(my_list):
-        my_list[idx] = ELEMENT
-    return(MY_List)
+def element_at(my_list, idx):
+    if idx < 0:
+        return None
+    elif idx >= len(my_list):
+        return None
+    return my_list[idx]
